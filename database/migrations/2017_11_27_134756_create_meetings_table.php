4ed25a64bb4a8a53c3bbe1e600cc72305077e3f1
Jan Zarsky <jzarsky@localhost.localdomain>
2017-11-27 15:35:35 +0100

    add test data

---
+++ database/migrations/2017_11_27_134756_create_meetings_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMeetingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('meetings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('alcoholic_id');
+            $table->integer('patron_id');
+            $table->date('date');
+            $table->string('location', 200);
+            $table->timestamps();
+
+            // TODO
+            /*
+            $table->foreign('alcoholic_id')
+                   ->references('id')
+                   ->on('users');
+            $table->foreign('patron_id')
+                   ->references('id')
+                   ->on('users');
+            */
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('meetings');
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        // Users
+        DB::table('users')->insert([
+            'name' => 'batman',
+            'email' => 'batman@gmail.com',
+            'password' => 'asdf',
+        ]);
+
+        // Meetings
+        DB::table('meetings')->insert([
+            'alcoholic_id' => 1,
+            'patron_id' => 1,
+            'date' => '2017-11-27',
+            'location' => 'Some cafe',
+        ]);
+
     }
 }
