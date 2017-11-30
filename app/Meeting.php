008c130b70e10bb6963f8801cd280496663eba3c
Jan Zarsky <jan.zarsky@gmail.com>
2017-11-30 17:40:44 +0100

    use soft deletes

--- app/Meeting.php
+++ app/Meeting.php
@@ -3,7 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Meeting extends Model
 {
+    use SoftDeletes;
+
+    protected $dates = ['deleted_at'];
 }
