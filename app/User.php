dc4f857138b32a57c4f1cdc95769e15913c44e6b
Jan Zarsky <jan.zarsky@gmail.com>
2017-12-04 14:21:06 +0100

    update user model

--- app/User.php
+++ app/User.php
@@ -12,15 +12,6 @@ class User extends Authenticatable
     use SoftDeletes;
 
     /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
-
-    /**
      * The attributes that should be hidden for arrays.
      *
      * @var array
